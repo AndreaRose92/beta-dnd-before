5c39f7b841c95a6ba2993eadeba03b8001e264e0
Andrea Rosentel <andrearosentel@gmail.com>
2022-09-29 12:11:50 -0700

    cleaned up serializers

--- app/serializers/game_info/race_serializer.rb
+++ app/serializers/game_info/race_serializer.rb
@@ -2,4 +2,13 @@ class RaceSerializer < ActiveModel::Serializer
   attributes :id, :name, :url, :size, :languages, :traits, :speed, :Strength, :Dexterity, :Constitution, :Intelligence, :Wisdom, :Charisma
   has_many :proficiencies
   has_many :subraces
+  def attributes(*args)
+    hash = super
+    hash.each {|k,v|
+      if v.nil?
+        hash.delete(k)
+      end
+    }
+    hash
+  end
 end
